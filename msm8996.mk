d423f537a04482ac7574038a6cb197cac8c7c283
xyyx <xyyx@mail.ru>
2018-09-25 17:00:21 +0800

    msm8996-common: Add health 2.0 service
    Change-Id: Id01cb4019982a05e2701a4c4ca6f0535ad136dc2
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -223,10 +223,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
     $(LOCAL_PATH)/gps/etc/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
-# Healthd
+# Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.0-service.gemini
 
 # HIDL
 PRODUCT_PACKAGES += \

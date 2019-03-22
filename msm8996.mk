3d6770aed1314a40154186c768845b4047c294de
mhkjahromi <m.h.k.jahromi@gmail.com>
2019-03-22 17:10:42 +0430

    msm8996: Add spectrum packages

--- msm8996.mk
+++ msm8996.mk
@@ -152,7 +152,9 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.power.rc \
     init.qcom.usb.rc \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+	init.spectrum.rc \
+    init.spectrum.sh
 
 # Display
 PRODUCT_PACKAGES += \

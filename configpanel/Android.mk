0ca3fc96af56887c1f0343a4a3faaa9a9d06067d
mhkjahromi <m.h.k.jahromi@gmail.com>
2019-03-27 13:56:18 +0430

    msm8996-common: fix configpanel

--- configpanel/Android.mk
+++ configpanel/Android.mk
@@ -6,27 +6,24 @@ LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PACKAGE_NAME := ConfigPanel
 LOCAL_PRIVATE_PLATFORM_APIS := true
-LOCAL_REQUIRED_MODULES := XiaomiPocketMode
 
+LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_USE_AAPT2 := true
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
-    android-support-v4 \
+    android-support-v14-preference \
+    android-support-v13 \
     android-support-v7-appcompat \
     android-support-v7-preference \
     android-support-v7-recyclerview \
-    android-support-v13 \
-    android-support-v14-preference \
+    android-support-v4
 
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    org.lineageos.platform.internal
-
-LOCAL_RESOURCE_DIR := \
-    $(LOCAL_PATH)/res \
-    $(TOP)/packages/resources/devicesettings/res
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_MODULE_TAGS := optional
+
+include frameworks/base/packages/SettingsLib/common.mk
 
 include $(BUILD_PACKAGE)
---
+++ configpanel/README.md
@@ -0,0 +1,1 @@
+Configpanel for xiaomi msm-8996 devices modified for android pie

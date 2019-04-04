5b80eb878b787aae201a3d9e66d58dc61f0c998e
Kazakov Stepan <ksrt12group@gmail.com>
2019-04-05 00:27:35 +0300

    Merge tag 'LA.UM.7.5.2.r1-02000-8x96.0' from https://source.codeaurora.org/quic/la/device/qcom/common

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -169,3 +169,43 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0
+
+#Additional props
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.qti.cgroup_follow.enable=true \
+    persist.vendor.qti.inputopts.enable=true \
+    persist.vendor.qti.inputopts.movetouchslop=0.6 \
+    ro.qcom.adreno.qgl.ShaderStorageImageExtendedFormats=0 \
+    sys.use_fifo_ui=1
+
+#LMKD props
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.lmk.low=1001 \
+    ro.lmk.medium=800 \
+    ro.lmk.critical=0 \
+    ro.lmk.critical_upgrade=false \
+    ro.lmk.upgrade_pressure=100 \
+    ro.lmk.downgrade_pressure=100 \
+    ro.lmk.kill_heaviest_task=true
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.keystore=msm8996
+
+# ro.location properties below are used in conjunction
+# with NLP_MODE setting in /vendor/etc/izat.conf
+# either as the main network provider or as a fallback
+# provider depending on NLP_MODE that is set
+# These properties are optional and will not cause
+# issues if not set to existing packages.
+# ro.location.osnlp.package sets the name of default
+# OS sepecific network location provider package to use
+# ro.location.osnlp.region.package is set to the name
+# of the alternative network location provider package
+# to be used for a particular region where the default
+# network location provider is not functional
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.location.osnlp.package=com.google.android.gms \
+    ro.location.osnlp.region.package=none
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.mod.romtags=droid=9.0.0_r35,caf=LA.UM.7.5.2.r1-02000-8x96.0

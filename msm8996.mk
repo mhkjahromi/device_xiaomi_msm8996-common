3d5f6040e4537f51030971c8078b082847117aa0
mhkjahromi <m.h.k.jahromi@gmail.com>
2019-08-14 12:23:16 +0430

    msm8996-common: extra: ringtone: Add Mi ringtone

--- msm8996.mk
+++ msm8996.mk
@@ -285,6 +285,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
 
+# Mi Ringtone
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/extra/ringtone/Mi.ogg:system/media/audio/ringtones/Mi.ogg
+	
 # Net
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.0 \

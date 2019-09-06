3687557e3d7164de0df2a6d8e422ffd99136a784
mhkjahromi <m.h.k.jahromi@gmail.com>
2019-09-06 14:51:36 +0430

    msm8996-common: Nuke sepolicy
    Use permissive sepolicy
    
    Signed-off-by: mhkjahromi <m.h.k.jahromi@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -200,9 +200,9 @@ TARGET_RIL_VARIANT := caf
 VENDOR_SECURITY_PATCH := 2019-04-05
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+# include device/qcom/sepolicy/sepolicy.mk
 
-BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+#BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
 # Vendor init
 TARGET_INIT_VENDOR_LIB := libinit_msm8996
